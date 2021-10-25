4f622acf299be1589c6b599883fae6566554fc17
AdrianSK <56803219+AdrianSK75@users.noreply.github.com>
2021-10-25 03:45:42 +0300

    Update DictionaryForm.jsx

--- src/components/DictionaryForm.jsx
+++ src/components/DictionaryForm.jsx
@@ -1,21 +1,5 @@
 import React, {Component} from 'react';
-
-class DictionaryList extends Component {
-    render() {
-        let dexURL = 'https://dexonline.ro/definitie/';
-        return (
-            <ol>
-                {this.props.items.map(item => (
-                    <li> 
-                        {item} 
-                        <a href ='#' onClick = {this.props.onDelete.bind(null, item)}>[Delete]</a>
-                        <a href ={dexURL + item} >[Search]</a>
-                    </li> 
-                ))}
-            </ol>
-        );
-    }
-}
+import DictionaryList from '../components/DictionaryList';
 
 class DictionaryForm extends Component {
         constructor(props) {
